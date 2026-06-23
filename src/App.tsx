a89f493fa7483af42a1cfbdc6ee7db7c5e0005d8
ajaybhayani741 <ajaybhayani741@gmail.com>
2026-06-23 18:37:14 +0530

    MT-1283 [Update message origin validation in App component]

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ const App = () => {
 
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
-      if (event.origin !== "http://localhost:3000") {
-        // if (event.origin !== "https://stg-matnext-in-uat.genbanext.com") {
+      // if (event.origin !== "http://localhost:3000") {
+        if (event.origin !== "https://stg-matnext-in-uat.genbanext.com") {
         return;
       }
 
